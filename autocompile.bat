6a8dbca9a9a7684e6917dd52fddbb1bdde87fd0b
jackik <jackik1410@users.noreply.github.com>
2018-04-07 04:21:21 +0200

    auto mod.zip compilation batch works
    fixed auto compilation batch file.
    currently only adds files.
    optional complete recreation via argument planned

--- autocompile.bat
+++ autocompile.bat
@@ -3,20 +3,21 @@ cls
 cd ..
 set Folder=../WW1-and-Future
 set archive=ww1-and-future.zip
-set 7zip=C:\Program Files\7-Zip\7zG.exe
+set SevenZip=C:/Program Files\7-Zip\7zG.exe
 
-if not exist %7zip% (
+if not exist "%SevenZip%" (
 echo 7zip installation not found!
 echo.
-echo there should be a file at %7zip%
+echo there should be a file at %SevenZip%
 echo errorlevel: %errorlevel%
 goto exit
 ) else (
 echo 7zip found
-timeout 1
+timeout 3
+cls
 )
 
-%7zip% a %archive% %Folder% -x!*.md -x!*.mod -x!.gitattributes -x!*.bat
+%SevenZip% a %archive% %Folder% -x!*.md -x!*.mod -x!.gitattributes -x!*.bat
 
 
 cls
