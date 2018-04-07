bb6407601c379a6160ff914680c46d3ec2219e42
jackik <jackik1410@users.noreply.github.com>
2018-04-07 04:11:32 +0200

    removed " in autocopy
    removed "comments"

--- autocopy.bat
+++ autocopy.bat
@@ -9,19 +9,14 @@ if not exist %7zip% (
 echo 7zip installation not found!
 echo.
 echo there should be a file at %7zip%
-echo error: %errorlevel%
+echo errorlevel: %errorlevel%
 goto exit
 ) else (
 echo 7zip found
 timeout 1
 )
 
-"%7zip% a %archive% %Folder% -x!*.md -x!*.mod -x!.gitattributes -x!*.bat
-
-#"C:\Program Files\7-Zip\7z" a -tzip archive.zip -r src\*.cpp src\*.h -x
-# H:\SteamLibrary\steamapps\workshop\content\394360\741148992
-
-
+%7zip% a %archive% %Folder% -x!*.md -x!*.mod -x!.gitattributes -x!*.bat
 
 
 cls
