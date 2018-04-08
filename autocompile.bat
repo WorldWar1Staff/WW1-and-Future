4f886dbfcd15fe9698c64926f000ff31fd7ec5ab
jackik <jackik1410@users.noreply.github.com>
2018-04-08 21:19:32 +0200

    added comments to .bat
    explains file locations

--- autocompile.bat
+++ autocompile.bat
@@ -1,9 +1,15 @@
 echo off
 cls
 cd ..
+::File locations to configure
 set Folder=../WW1-and-Future
+::the folder to be transfered
 set archive=ww1-and-future.zip
+::found under %SteamDirectory%\steamapps\workshop\content\394360\741148992\WW1-and-Future
 set SevenZip=C:/Program Files\7-Zip\7zG.exe
+::Location of the 7zip installation
+
+::do not edit past this!
 
 if not exist "%SevenZip%" (
 echo 7zip installation not found!
@@ -13,6 +19,7 @@ echo errorlevel: %errorlevel%
 goto exit
 ) else (
 echo 7zip found
+echo.
 timeout 3
 cls
 )
